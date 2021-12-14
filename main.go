89df66efd0bb2292fe313603bee1a041aee9f9f4
china-o <1025300632@qq.com>
2021-12-15 02:53:55 +0800

    feat: init

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"serve/util"
+	"time"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	disk_parts := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		// c.JSON(http.StatusOK, hom4)
+		html := util.GetVisitHTML()
+		c.Header("Content-Type", "text/html;charset=utf-8")
+		c.String(http.StatusOK, html)
+	})
+	for _, disk_part := range disk_parts {
+		r.StaticFS("/"+disk_part, http.Dir(disk_part+":"))
+	}
+	ip := util.GetLocalIP1()
+	srv := &http.Server{
+		Addr:    ":9999",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Println("please visit: ", fmt.Sprintf("http://%s:%d", ip, util.PORT))
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
+}
---
+++ util/util.go
@@ -0,0 +1,77 @@
+package util
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+const PORT = 9999
+
+func GetLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+
+}
+
+func GetLocalIP1() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	return conn.LocalAddr().(*net.UDPAddr).IP.To4().String()
+}
+
+func GetLogicalDrives() []string {
+	kernel32 := syscall.MustLoadDLL("kernel32.dll")
+	GetLogicalDrives := kernel32.MustFindProc("GetLogicalDrives")
+	n, _, _ := GetLogicalDrives.Call()
+	s := strconv.FormatInt(int64(n), 2)
+	var drives_all = []string{"A:", "B:", "C:", "D:", "E:", "F:", "G:", "H:", "I:", "J:", "K:", "L:", "M:", "N:", "O:", "P：", "Q：", "R：", "S：", "T：", "U：", "V：", "W：", "X：", "Y：", "Z："}
+	temp := drives_all[0:len(s)]
+	var d []string
+	for i, v := range s {
+		if v == 49 {
+			l := len(s) - i - 1
+			d = append(d, temp[l])
+		}
+	}
+	var drives []string
+	for i, v := range d {
+		drives = append(drives[i:], append([]string{v}, drives[:i]...)...)
+	}
+	return drives
+}
+
+func GetVisitHTML() string {
+	template := ""
+	ip := GetLocalIP1()
+	port := PORT
+	http_host := fmt.Sprintf("http://%s:%d", ip, port)
+	drives := GetLogicalDrives()
+	for _, drive := range drives {
+		d := strings.ToLower(drive)
+		d = d[0:1]
+		fmt.Println(d)
+		link := http_host + "/" + d + "/"
+		node := fmt.Sprintf("<h3><a href='%s'>%s</a></h3>", link, d)
+		fmt.Println(node)
+		template += node
+	}
+	return template
+}
